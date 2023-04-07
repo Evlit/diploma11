99b36904e703be92580851dcf6c28f5cccc04dae
Evlit <evlit23@gmail.com>
2023-04-07 12:10:39 +0300

    Lesson 37(all first commit)

--- goals/admin.py
+++ goals/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from goals.models import GoalCategory, Goal
+from goals.models import GoalCategory, Goal, GoalComment, BoardParticipant
 
 
 @admin.register(GoalCategory)
@@ -14,3 +14,18 @@ class GoalAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
     search_fields = ('title', 'description')
     list_filter = ('status', 'priority')
+
+
+@admin.register(GoalComment)
+class GoalCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'goal', 'text')
+    search_fields = ('user', 'goal')
+    list_filter = ('user',)
+
+
+# admin.site.register(BoardParticipant)
+@admin.register(BoardParticipant)
+class BoardParticipantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'board')
+    search_fields = ('user', 'role')
+    list_filter = ('user',)
